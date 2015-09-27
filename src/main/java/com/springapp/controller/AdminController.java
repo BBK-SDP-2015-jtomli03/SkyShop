fd8faf2e9072cc28bba8863245320a1b870fd4a4
jtomli03 <jotom1000@gmail.com>
2015-09-27 20:28:18 +0100

    Admin Controller added

---
+++ src/main/java/com/springapp/controller/AdminController.java
@@ -0,0 +1,17 @@
+package com.springapp.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ * Created by Jo on 27/09/2015.
+ */
+@Controller
+@RequestMapping("/admin")
+public class AdminController {
+    @RequestMapping(method = RequestMethod.GET)
+    public String printWelcome() {
+        return "admin";
+    }
+}
